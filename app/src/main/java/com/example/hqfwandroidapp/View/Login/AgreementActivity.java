86d47fa68ab8bbb3f2cf897f47d8fc9aad96248c
lemon-laptop-local <lemeng1@qq.com>
2019-01-17 22:21:23 +0800

    建立了登录、注册、手机归属地，协议的 Activity 。

---
+++ app/src/main/java/com/example/hqfwandroidapp/View/Login/AgreementActivity.java
@@ -0,0 +1,15 @@
+package com.example.hqfwandroidapp.View.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hqfwandroidapp.R;
+
+public class AgreementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agreement);
+    }
+}

fd640ab7efcfa97dbadace0bb8da4980292625b1
lemon-laptop-local <lemeng1@qq.com>
2019-01-26 14:38:18 +0800

    完成了：将项目迁移至 AndroidX。

--- app/src/main/java/com/example/hqfwandroidapp/Login/LoginActivity.java
+++ app/src/main/java/com/example/hqfwandroidapp/Login/LoginActivity.java
@@ -1,6 +1,6 @@
 package com.example.hqfwandroidapp.Login;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +15,7 @@ import com.lzy.okgo.callback.StringCallback;
 import com.lzy.okgo.model.Response;
 
 import butterknife.BindView;
+
 
 public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.et_phone_login) EditText etPhone;

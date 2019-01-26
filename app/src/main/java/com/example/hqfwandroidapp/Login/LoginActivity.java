0786fcb2c9e0cf2ac55e23dda28d79f78d4b339f
lemon-laptop-local <lemeng1@qq.com>
2019-01-26 15:23:01 +0800

    完成了：能够正常启动项目。

--- app/src/main/java/com/example/hqfwandroidapp/Login/LoginActivity.java
+++ app/src/main/java/com/example/hqfwandroidapp/Login/LoginActivity.java
@@ -15,59 +15,55 @@ import com.lzy.okgo.callback.StringCallback;
 import com.lzy.okgo.model.Response;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.et_phone_login) EditText etPhone;
     @BindView(R.id.et_password_login) EditText etPassword;
-    @BindView(R.id.btn_login) Button btnLogin;
+    //@BindView(R.id.btn_login) Button btnLogin;
     @BindView(R.id.tv_find_password) TextView tvFindPassword;
     @BindView(R.id.tv_register) TextView tvRegister;
 
+    @OnClick(R.id.btn_login) void submit() {
+        String phone = etPhone.getText().toString();
+        String password = etPassword.getText().toString();
+        OkGo.<String>post(Urls.getLogin())
+                .params("phone", phone)
+                .params("password", password)
+                .execute(new StringCallback() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String phone = etPhone.getText().toString();
-                String password = etPassword.getText().toString();
-                OkGo.<String>post(Urls.getLogin())
-                        .params("phone", phone)
-                        .params("password", password)
-                        .execute(new StringCallback() {
                     @Override
                     public void onSuccess(Response<String> response) {
-                        Toast toast = Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT);
+                        Toast toast = Toast.makeText(getApplication().getApplicationContext(), response.toString(), Toast.LENGTH_SHORT);
                         toast.show();
                     }
 
                     @Override
                     public void onError(Response<String> response) {
                         super.onError(response);
-                        //Toast toast = Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT);
-                        //toast.show();
+                        Toast toast = Toast.makeText(getApplication().getApplicationContext(), "onError"+response.toString(), Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        super.onFinish();
+                        Toast toast = Toast.makeText(getApplication().getApplicationContext(), "onFinish", Toast.LENGTH_SHORT);
+                        toast.show();
                     }
                 });
-            }
-        });
+    }
 
-        tvFindPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // 绑定生效
+        ButterKnife.bind(this);
 
-            }
-        });
 
-        tvRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
 
     }
 

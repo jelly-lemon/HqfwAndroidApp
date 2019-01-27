64c6f4f3233bb3eb05b2f0430050f21328f60bfd
lemon-laptop-local <lemeng1@qq.com>
2019-01-27 11:49:51 +0800

    完成了：利用 SharedPreference 实现了自动登录。

--- app/src/main/java/com/example/hqfwandroidapp/Login/LoginActivity.java
+++ app/src/main/java/com/example/hqfwandroidapp/Login/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import com.example.hqfwandroidapp.MainActivity;
 import com.example.hqfwandroidapp.R;
+import com.example.hqfwandroidapp.SaveSharedPreference;
 import com.example.hqfwandroidapp.Urls;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
@@ -29,24 +30,8 @@ public class LoginActivity extends AppCompatActivity {
         String phone = etPhone.getText().toString();
         String password = etPassword.getText().toString();
 
-        OkGo.<String>post(Urls.getLogin())
-                .params("phone", phone)
-                .params("password", password)
-                .execute(new StringCallback() {
-                    @Override
-                    public void onSuccess(Response<String> response) {
-                        showToast(response.body());
-                        goMainActivity();
-                    }
-
-                    @Override
-                    public void onError(Response<String> response) {
-                        super.onError(response);
-                        showToast("网络故障，请检查网络");
-                    }
-
-
-                });
+        saveUser(phone, password);
+        login(phone, password);
     }
 
     @Override
@@ -55,6 +40,9 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         // ButterKnife 绑定生效
         ButterKnife.bind(this);
+        // 自动登录
+        autoLogin();
+
     }
 
 
@@ -68,4 +56,38 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
         this.finish();
     }
+
+    private void autoLogin() {
+        if (SaveSharedPreference.getPhone(this).length() != 0) {
+            String phone = SaveSharedPreference.getPhone(this);
+            String password = SaveSharedPreference.getPassword(this);
+            login(phone, password);
+        }
+    }
+
+    private void login(String phone, String password) {
+        OkGo.<String>post(Urls.getLogin())
+                .params("phone", phone)
+                .params("password", password)
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+
+                        showToast(response.body());
+                        goMainActivity();
+                    }
+
+                    @Override
+                    public void onError(Response<String> response) {
+                        super.onError(response);
+                        showToast("网络故障，请检查网络");
+                    }
+                });
+    }
+
+    private void saveUser(String phone, String password) {
+        SaveSharedPreference.setPhone(this, phone);
+        SaveSharedPreference.setPassword(this, password);
+    }
+
 }
